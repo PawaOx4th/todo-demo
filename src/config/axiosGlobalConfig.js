f9916f29e29ea2e5315f2f3fa533ca0739d34ec9
pawaox4th <pawaox4th@gmail.com>
2022-11-12 22:08:59 +0700

    refactor: 💡 Update axios

--- src/config/axiosGlobalConfig.js
+++ src/config/axiosGlobalConfig.js
@@ -1,8 +1,14 @@
 import axios from "axios"
 
+var myHeaders = new Headers()
+myHeaders.append("userkey", "usersecret")
+
 const http = axios.create({
-  baseURL: "https://jsonplaceholder.typicode.com"
-  //   baseURL: process.env.REACT_APP_BASE_URL_API,
+  // baseURL: "https://jsonplaceholder.typicode.com"
+  baseURL: process.env.REACT_APP_BASE_URL_API,
+  headers: {
+    userkey: "usersecret"
+  }
   //   withCredentials: false,
   //   headers: {
   //     "Access-Control-Allow-Origin": "*",
